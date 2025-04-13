a1c5a99098e478c20aa84d1d3d0fda245438d484
Ivan Nachev <ivannachev03@schoolmath.eu>
2025-04-13 20:38:24 +0300

    New Password Converter added

---
+++ UI/Extras/PasswordConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace UI.Extras
+{
+    public class PasswordConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string password && !string.IsNullOrEmpty(password))
+            {
+                return new string('*', password.Length);
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value; // За еднопосочни биндинги не е нужно да връщате стойност обратно
+        }
+    }
+}
